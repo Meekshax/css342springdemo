eb1f27f72bf5027d2ea3c1dfa87b0e59971e1a87
Meekshax <meekshathakur@gmail.com>
2025-04-04 12:29:04 -0700

    Exercise one and two

--- main.cpp
+++ main.cpp
@@ -1,37 +1,60 @@
 #include <iostream>
-#include <string>
+using namespace std;
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void test();
-class Fraction;
+/* Part of exercise one */
+bool is_power_of_2(int num){
+    while (num!=1) {
+        if (num%2==0){
+            num=num/2;
+            continue;
+        }
+        else {
+            return false;
+        }
+    }
+    return true;
+}
+ /* Part of exercise two */
+bool is_prime_number(int num){
+    if(num == 0 || num == 1){ //prime number cannot be 0 or 1
+        return false;
+    }
+    for(int i = 2; i <= num/2; ++i ){
+        if(num%i == 0){
+            return false;
+            break;
+        }
+    }
+    return true;
+}
+
+int main(){
+    /* Exercise one*/
+    cout << "Pleaser enter a number: " << endl;
+    int num;
+    cin >> num;
+    bool result = is_power_of_2(num);
+    if(result == true){
+        cout << "The given number is a power of 2";
+    } else {
+        cout << "The given number is not a power of 2";
+    }
+
+    /* Exercise two */
+    int num = 11;
+    bool result = is_prime_number(num);
+    if(result == true){
+        cout << "Pass!" << endl;
+    } else {
+        cout << "Fail!" << endl;
+    }
+
+    /* Exercise three */
 
 
 
-class Fraction {
-    public:
-    int numerator;
-    int denominator;
 
-    Fraction(int numerator, int denominator): numerator(numerator), denominator(denominator) { }
-
-
-};
-
-Fraction reduce(Fraction input); //a/b fraction
-
-int main() {
-    test();
-    Fraction num1(2, 4 );
-    std::cout << num1.to_string() << std::endl;
     return 0;
 }
-void test(){
-    //Reduce given fraction to minimum terms.
-    //e.g. 2/4 -> 1/2   6/6 -> 1/1   1/0 -> 0(error)
-}
 
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
+
